b3116e0d783f3bcc3b2cc716cb39af73275b3c93
Awesome Inc <awesomeinc@Awesome-Isaac-MBP.local>
2018-09-21 14:58:20 -0400

    Removed thankyou.html, updated donation slider and button

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-function redirect() {
-  var response = document.getElementById('answer').value;
+function toDollars() {
+    let dollars = document.getElementById('range').value;
+    document.getElementById('output').value = '$' + dollars;
+}
+
+function thankYou() {
+  let response = document.getElementById('answer').value;
+  let donation = document.getElementById('range').value;
   if (response === "Yes") {
-    window.location.href = "thankyou.html";
-  } else {
+    if (donation > 0) {
+      alert("Thank you for your generous donation, and pledge. DON'T FORGET TO VOTE ON NOVEMBER 6TH!");
+    } else {
+      alert("Thank you for your pledge. DON'T FORGET TO VOTE ON NOVERMBER 6TH!");
+    }
+  } else if (response === "No"){
     window.location.href = "https://www.google.com";
   }
-  return false;
 }
